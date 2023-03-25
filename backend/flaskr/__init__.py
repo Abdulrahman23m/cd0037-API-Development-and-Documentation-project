1d2fbc6daa1774d50ad660284d921c4dc4e202ad
Abdulrahman23m <Abdulrahman-almejmaj@outlook.com>
2023-03-25 22:56:13 +0300

    some functions

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -7,39 +7,75 @@ import random
 from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
+def paginate_question(request, selection):
+    page = request.args.get("page", 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    books = [book.format() for book in selection]
+    current_books = books[start:end]
+
+    return current_books
+
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
-    setup_db(app)
-
-    """
-    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-    """
-
-    """
-    @TODO: Use the after_request decorator to set Access-Control-Allow
-    """
-
-    """
-    @TODO:
-    Create an endpoint to handle GET requests
-    for all available categories.
-    """
+    with app.app_context():
+        setup_db(app)
+    CORS(app)
 
 
-    """
-    @TODO:
-    Create an endpoint to handle GET requests for questions,
-    including pagination (every 10 questions).
-    This endpoint should return a list of questions,
-    number of total questions, current category, categories.
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+        return response
 
-    TEST: At this point, when you start the application
-    you should see questions and categories generated,
-    ten questions per page and pagination at the bottom of the screen for three pages.
-    Clicking on the page numbers should update the questions.
-    """
+    @app.route("/categories")
+    def get_categories():
+        selection = Category.query.order_by(Category.id).all()
+
+        if len(selection)==0:
+            abort(404)
+
+        return jsonify({
+            "success": True,
+            "categories": {category.id: category.type for category in selection},
+            # 'categories':  [categories.format() for categories in selection],
+            "total_categories":len(selection)
+        })
+
+    @app.route("/questions")
+    def get_questions():
+        selection = Question.query.order_by(Question.id).all()
+        current_questions = paginate_question(request,selection)
+        if len(current_questions)==0:
+            abort(404)
+        categories = Category.query.order_by(Category.id).all()
+        print() 
+        return jsonify({
+            "success": True,
+            "questions": current_questions,
+            "total_questions":len(current_questions),
+            "categories": {category.id: category.type for category in categories}
+        })
+
+    @app.route('/questions/<int:question_id>', methods=["DELETE"])
+    def delete_question(question_id):
+        try:
+            question = Question.query.get(question_id)
+            print(question)
+
+            if question is None:
+                abort(404)
+
+            question.delete()
+
+            return jsonify({"success": True})
+        except:
+            abort(422)
 
     """
     @TODO:
@@ -48,7 +84,34 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
+    @app.route('/questions',methods=['POST'])
+    def add_question():
+        body = request.get_json()
+        print(body)
+        new_question = body.get('question',None)
+        new_answer = body.get('answer',None)
+        new_category = body.get('category',None)
+        new_difficulty = body.get('difficulty',None)
 
+        try:
+            question = Question(question=new_question,answer=new_answer,category=new_category,difficulty=new_difficulty)
+            question.insert()
+            selection = Question.query.order_by(Question.id).all()
+            current_questions = paginate_question(request,selection)
+            if len(current_questions)==0:
+                abort(404)
+            categories = Category.query.all()
+            # categories = [category.format() for category in categories]
+
+            return jsonify({
+            "success": True,
+            # "questions": current_questions,
+            # "total_questions":len(current_questions),
+            # "categories": categoriesDict,
+            "created": question.id
+            })
+        except:
+            abort(422)
     """
     @TODO:
     Create an endpoint to POST a new question,
@@ -92,11 +155,19 @@ def create_app(test_config=None):
     and shown whether they were correct or not.
     """
 
-    """
-    @TODO:
-    Create error handlers for all expected errors
-    including 404 and 422.
-    """
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return (
+            jsonify({"success": False, "error": 404, "message": "resource not found"}),
+            404,
+        )
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return (
+            jsonify({"success": False, "error": 422, "message": "unprocessable"}),
+            422,
+        )
 
     return app
 
--- backend/models.py
+++ backend/models.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = 'trivia'
-database_path = 'postgresql://{}/{}'.format('localhost:5432', database_name)
-
+database_path = 'postgresql://{}/{}'.format('postgres:baba12@localhost:5432', database_name)
+print(database_path)
 db = SQLAlchemy()
 
 """
